3aae5b89a0817fea0e04a8573ec9431f72bed5d8
Aiden Gardner <aiden.gardner@wisetechglobal.com>
2024-02-09 14:01:06 +1100

    Add terrain api endpoints to Terrain API client

--- Crest/Utilities/TerrainAPIClient.cs
+++ Crest/Utilities/TerrainAPIClient.cs
@@ -1,14 +1,21 @@
-﻿using System.Net.Http.Headers;
+﻿using Newtonsoft.Json.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Text.Json;
 
 namespace Crest.Utilities
 {
-	public static class TerrainAPIClient
+	public class TerrainAPIClient
 	{
-		public static HttpResponseMessage Login(string username, string password)
+		readonly HttpClient Client;
+
+		public TerrainAPIClient()
 		{
-			var client = new HttpClient();
+			Client = new HttpClient();
+		}
+
+		public bool Login(string username, string password, out string loginFailureReason)
+		{
 			var url = "https://cognito-idp.ap-southeast-2.amazonaws.com";
 
 			var body = new
@@ -28,7 +35,54 @@ namespace Crest.Utilities
 			request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/x-amz-json-1.1");
 			request.Content.Headers.Add("X-Amz-Target", "AWSCognitoIdentityProviderService.InitiateAuth");
 
-			return client.Send(request);
+			var response = Client.Send(request);
+			var responseJson = JObject.Parse(response.Content.ReadAsStringAsync().Result);
+			
+			if (responseJson.TryGetValue("AuthenticationResult", out var authTokenResult))
+			{
+				Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(authTokenResult["IdToken"].ToString());
+
+				loginFailureReason = null;
+
+				return true;
+			}
+
+			if (responseJson.TryGetValue("__type", out var type) && responseJson.TryGetValue("message", out var message))
+			{
+				loginFailureReason = $"{type}: {message}";
+				return false;
+			}
+
+			loginFailureReason = "An unknown error occurred: " + responseJson.ToString();
+			return false;
+		}
+
+		public IEnumerable<JToken> GetPendingApprovals(string unitID)
+			=> GetResultsList($"https://achievements.terrain.scouts.com.au/units/{unitID}/submissions?status=pending");
+
+		public IEnumerable<JToken> GetFinalisedApprovals(string unitID)
+			=> GetResultsList($"https://achievements.terrain.scouts.com.au/units/{unitID}/submissions?status=finalised");
+
+		public IEnumerable<JToken> GetMemberAchievements(string memberID)
+			=> GetResultsList($"https://achievements.terrain.scouts.com.au/members/{memberID}/achievements");
+
+		IEnumerable<JToken> GetResultsList(string url)
+		{
+			var response = Client.SendAsync(new HttpRequestMessage(HttpMethod.Get, url)).Result;
+			Console.WriteLine(url);
+
+			if (!response.IsSuccessStatusCode)
+			{
+				return new List<JToken>();
+			}
+
+			var responseJson = JObject.Parse(response.Content.ReadAsStringAsync().Result);
+			if (responseJson.TryGetValue("results", out var results))
+			{
+				return results.ToObject<IEnumerable<JToken>>();
+			}
+
+			return null;
 		}
 	}
 }
