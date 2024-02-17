c29b3033d4e118c9bfebea6f7549b6046a67014a
Aiden Gardner <aiden.gardner@wisetechglobal.com>
2024-02-17 19:41:25 +1100

    I hate moq

--- Crest.Test/ScoutEventCrawlerTaskTests.cs
+++ Crest.Test/ScoutEventCrawlerTaskTests.cs
@@ -3,6 +3,7 @@ using Crest.Integration;
 using Crest.Test.Utilities;
 using Crest.Utilities;
 using Moq;
+using Newtonsoft.Json;
 using NUnit.Framework;
 
 namespace Crest.Test;
@@ -12,59 +13,72 @@ public class ScoutEventCrawlerTaskTests : DeleteProgramDataBeforeTest
 	[Test]
 	public void TestOneRegion_NoPreviouslySeenEvents()
 	{
-		// Arrange input data
+		// Arrange
+		CreateMocks(out var task, out var mockJandiClient, out var mockScoutEventClient);
+
 		var taskConfig = new ScoutEventCrawlerTaskConfig()
 		{
 			JandiUrl = "https://jandiurl.com",
 			SubscribedRegions = { SubscribableRegion.south_metropolitan }
 		};
 
-		var mockRegion = new Region("South Metropolitan Region", "https://events.nsw.scouts.com.au/region/sm", new List<Event>
+		var mockSouthMetRegion = new Region("South Metropolitan Region", "https://events.nsw.scouts.com.au/region/sm", new List<Event>
 		{
 			new("Paddling Day", 123, "Saturday 23rd December to Sunday 24th December", "Open", "success"),
 			new("Rock Climbing", 456, "Sunday 1st Jan to Monday 2nd Jan", "Closed", "danger"),
 			new("Abseiling", 789, "March 15th", "Open (Restricted)", "warning"),
 		});
 
-		// Arrange mocks
-		var mockScoutEventClient = new Mock<ScoutEventAPIClient>();
-		var mockJandiClient = new Mock<JandiAPIClient>();
-		var task = MockScoutEventCrawlerTask(mockJandiClient, mockScoutEventClient);
+		var expectedJandiMessage = new JandiMessage()
+		{
+			Body = "New ScoutLink events",
+			ConnectColor = "#84BC48",
+			ConnectInfo = new List<JandiConnect>
+			{
+				new() { Title = "⚜️ [South Metropolitan Region](https://events.nsw.scouts.com.au/region/sm)", Description = "[Paddling Day](https://events.nsw.scouts.com.au/event/123) • Open ✅\nSaturday 23rd December to Sunday 24th December\n\n[Abseiling](https://events.nsw.scouts.com.au/event/789) • Open (Restricted) ⚠️\nMarch 15th" },
+			}
+		};
 
-		mockScoutEventClient.Setup(c => c.ScanRegion(SubscribableRegion.south_metropolitan)).Returns(mockRegion);
-		mockJandiClient.Setup(c => c.SendMessage(It.IsAny<string>(), It.IsAny<JandiMessage>()))
-			.Callback((string url, JandiMessage message) => AssertJandiMessagePosted(url, message));
+		mockScoutEventClient.Setup(c => c.ScanRegion(SubscribableRegion.south_metropolitan)).Returns(mockSouthMetRegion);
 
 		// Act
 		task.Run(taskConfig);
 
 		// Assert
-		mockJandiClient.Verify(c => c.SendMessage(It.IsAny<string>(), It.IsAny<JandiMessage>()), Times.Once);
+		mockJandiClient.Verify(c => c.SendMessage(
+			taskConfig.JandiUrl,
+			It.Is((JandiMessage message) => Serialise(message) == Serialise(expectedJandiMessage))
+		), Times.Once);
 	}
 
-	private static ScoutEventCrawlerTask MockScoutEventCrawlerTask(Mock<JandiAPIClient> mockJandiClient, Mock<ScoutEventAPIClient> mockScoutEventClient)
+	#region Helpers
+
+	private static void CreateMocks(out ScoutEventCrawlerTask task, out Mock<JandiAPIClient> mockJandiClient, out Mock<ScoutEventAPIClient> mockScoutEventClient, bool verbose = false)
 	{
+		mockScoutEventClient = new Mock<ScoutEventAPIClient>();
+		mockJandiClient = new Mock<JandiAPIClient>();
 		var mockTask = new Mock<ScoutEventCrawlerTask>(mockJandiClient.Object, mockScoutEventClient.Object) { CallBase = true };
+		task = mockTask.Object;
 
 		mockTask.SetupGet(m => m.StatePath).Returns(ProgramDataLocation);
 		mockTask.SetupGet(t => t.StateKey).Returns("crawler1-ScoutEventCrawlerTask");
 
-		return mockTask.Object;
-	}
-
-	private static void AssertJandiMessagePosted(string url, JandiMessage message)
-	{
-		Assert.Multiple(() =>
+		if (verbose)
 		{
-			Assert.That(url, Is.EqualTo("https://jandiurl.com"));
-
-			Assert.That(message.Body, Is.EqualTo("New ScoutLink events"));
-			Assert.That(message.ConnectColor, Is.EqualTo("#84BC48"));
-			Assert.That(message.ConnectInfo, Has.Count.EqualTo(1));
-
-			var connect = message.ConnectInfo[0];
-			Assert.That(connect.Title, Is.EqualTo("⚜️ [South Metropolitan Region](https://events.nsw.scouts.com.au/region/sm)"));
-			Assert.That(connect.Description, Is.EqualTo("[Paddling Day](https://events.nsw.scouts.com.au/event/123) • Open ✅\nSaturday 23rd December to Sunday 24th December\n\n[Abseiling](https://events.nsw.scouts.com.au/event/789) • Open (Restricted) ⚠️\nMarch 15th"));
-		});
+			mockJandiClient
+				.Setup(c => c.SendMessage(It.IsAny<string>(), It.IsAny<JandiMessage>()))
+				.Callback(JandiCallback);
+		}
 	}
+
+	private static string Serialise(JandiMessage message)
+		=> JsonConvert.SerializeObject(message);
+
+	private static void JandiCallback(string url, JandiMessage message)
+	{
+		var formattedMessage = JsonConvert.SerializeObject(message, Formatting.Indented);
+		Console.WriteLine($"SendMessage called for url {url}\nmessage {formattedMessage}");
+	}
+
+	#endregion
 }
