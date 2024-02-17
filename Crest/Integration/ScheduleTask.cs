730851d7e87f95d87f6aee3213a54425610f778f
Aiden Gardner <aiden.gardner@wisetechglobal.com>
2024-02-17 12:20:18 +1100

    Rename T to TConfig

--- Crest/Integration/ScheduleTask.cs
+++ Crest/Integration/ScheduleTask.cs
@@ -3,30 +3,30 @@ using Quartz;
 
 namespace Crest.Integration
 {
-	public abstract class ScheduleTask<T> : IJob where T : ITaskConfig
+	public abstract class ScheduleTask<TConfig> : IJob where TConfig : ITaskConfig
 	{
 		private IJobExecutionContext Context;
 
 		/// <summary>
-		/// Runs just before <see cref="Run(T)"/> and returns a bool indicating whether any further instances of this task should run.
+		/// Runs just before <see cref="Run(TConfig)"/> and returns a bool indicating whether any further instances of this task should run.
 		/// For example, returns false if credentials have become invalid.
 		/// </summary>
-		/// <param name="config">Configuration object of type <see cref="T"/> for this task</param>
+		/// <param name="config">Configuration object of type <see cref="TConfig"/> for this task</param>
 		/// <returns></returns>
-		public virtual bool ShouldRun(T config) => true;
+		public virtual bool ShouldRun(TConfig config) => true;
 
 		/// <summary>
-		/// The main run function for this task. Run only if <see cref="ShouldRun(T)"/> returns true. <paramref name="config"/> has been pre-parsed
+		/// The main run function for this task. Run only if <see cref="ShouldRun(TConfig)"/> returns true. <paramref name="config"/> has been pre-parsed
 		/// </summary>
-		/// <param name="config">Configuration object of type <see cref="T"/> for this task</param>
-		public abstract void Run(T config);
+		/// <param name="config">Configuration object of type <see cref="TConfig"/> for this task</param>
+		public abstract void Run(TConfig config);
 
 		public Task Execute(IJobExecutionContext context)
 		{
 			Context = context;
 
 			var configString = context.MergedJobDataMap.GetString("config");
-			var config = JsonConvert.DeserializeObject<T>(configString);
+			var config = JsonConvert.DeserializeObject<TConfig>(configString);
 
 			if (!ShouldRun(config))
 			{
