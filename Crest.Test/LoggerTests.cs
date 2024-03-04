6e7e1c1b20929bd614605a67e1cf96cf9df5fe6b
Aiden Gardner <aiden.gardner@wisetechglobal.com>
2024-03-04 11:48:22 +1100

    Add logger with tests

---
+++ Crest.Test/LoggerTests.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+
+namespace Crest.Utilities;
+
+public class LoggerTests
+{
+	[Test]
+	public void TestLoggerCreatesFile_AndContentsFormatIsCorrect()
+	{
+		var fileSafeNow = DateTime.Now.ToString("dd-MMM-yy-HHmmss");
+		var logFilePath = Path.GetFullPath($"crest-{fileSafeNow}.log");
+
+		using (new SuppressStdout())
+		using (var logger = Logger.Instance)
+		{
+			Assert.That(File.Exists(logFilePath), Is.True);
+
+			logger.Debug("debug");
+			logger.Info("information");
+			logger.Warn("warning");
+			logger.Error("error");
+		}
+
+		Assert.Multiple(() =>
+		{
+			Assert.That(File.Exists(logFilePath), Is.True);
+
+			var logContents = File.ReadAllText(logFilePath).Split(Environment.NewLine);
+			Assert.That(logContents[0], Does.EndWith("[DEBUG] debug"));
+			Assert.That(logContents[1], Does.EndWith("[INFO]  information"));
+			Assert.That(logContents[2], Does.EndWith("[WARN]  warning"));
+			Assert.That(logContents[3], Does.EndWith("[ERROR] error"));
+		});
+	}
+
+	[Test]
+	public void TestDisposingAndCreatingNewLoggerCreatesNewLogFile()
+	{
+		var initialLoggerFilePath = Logger.Instance.FilePath;
+		
+		Logger.Instance.Dispose();
+		Thread.Sleep(1000);
+
+		Assert.That(Logger.Instance.FilePath, Is.Not.EqualTo(initialLoggerFilePath));
+	}
+}
+
+class SuppressStdout : IDisposable
+{
+	readonly TextWriter InitialConsoleOut;
+
+	public SuppressStdout()
+	{
+		InitialConsoleOut = Console.Out;
+		Console.SetOut(new StringWriter());
+	}
+
+	public void Dispose()
+	{
+		Console.Out.Dispose();
+		Console.SetOut(InitialConsoleOut);
+	}
+}
---
+++ Crest/Utilities/Logger.cs
@@ -0,0 +1,58 @@
+﻿namespace Crest.Utilities;
+
+public sealed class Logger : IDisposable
+{
+	public readonly string FilePath;
+	private readonly StreamWriter LogWriter;
+	private static Logger InternalInstance;
+
+	public static Logger Instance
+		=> InternalInstance ??= new();
+
+	private Logger()
+	{
+		var fileSafeNow = DateTime.Now.ToString("dd-MMM-yy-HHmmss");
+		var logFilePath = Path.GetFullPath($"crest-{fileSafeNow}.log");
+
+		FilePath = Path.GetFullPath(logFilePath);
+		LogWriter = new StreamWriter(FilePath);
+	}
+
+	public void Dispose()
+	{
+		InternalInstance = null;
+		LogWriter?.Dispose();
+	}
+
+	public static Logger CreateNewInstance()
+	{
+		InternalInstance?.Dispose();
+		InternalInstance = new Logger();
+
+		return InternalInstance;
+	}
+
+	public void Debug(string message)
+		=> AddLog("DEBUG", message);
+
+	public void Info(string message)
+		=> AddLog("INFO", message);
+
+	public void Warn(string message)
+		=> AddLog("WARN", message);
+
+	public void Error(string message)
+		=> AddLog("ERROR", message);
+
+	void AddLog(string severity, string message)
+	{
+		var prefix = $"{DateTime.Now} [{severity}]".PadRight(31);
+		var formattedMessage = $"{prefix} {message}";
+
+		lock (LogWriter)
+		{
+			Console.WriteLine(formattedMessage);
+			LogWriter.WriteLine(formattedMessage);
+		}
+	}
+}
