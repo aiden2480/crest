7f0edbcca96a0922940eb86c715b52ba5cfaa3ae
Aiden Gardner <aiden.gardner@wisetechglobal.com>
2024-03-04 12:23:19 +1100

    Refactor Logger

--- Crest/Utilities/Logger.cs
+++ Crest/Utilities/Logger.cs
@@ -2,12 +2,9 @@
 
 public sealed class Logger : IDisposable
 {
-	public readonly string FilePath;
 	private readonly StreamWriter LogWriter;
-	private static Logger InternalInstance;
-
-	public static Logger Instance
-		=> InternalInstance ??= new();
+	internal readonly string FilePath;
+	internal static Logger Instance;
 
 	private Logger()
 	{
@@ -20,39 +17,47 @@ public sealed class Logger : IDisposable
 
 	public void Dispose()
 	{
-		InternalInstance = null;
+		Instance = null;
 		LogWriter?.Dispose();
 	}
 
 	public static Logger CreateNewInstance()
 	{
-		InternalInstance?.Dispose();
-		InternalInstance = new Logger();
+		Instance?.Dispose();
+		Instance = new Logger();
 
-		return InternalInstance;
+		return Instance;
 	}
 
-	public void Debug(string message)
-		=> AddLog("DEBUG", message);
+	public static void Debug(string message, string category = null)
+		=> AddLog("DEBUG", message, category);
 
-	public void Info(string message)
-		=> AddLog("INFO", message);
+	public static void Info(string message, string category = null)
+		=> AddLog("INFO", message, category);
 
-	public void Warn(string message)
-		=> AddLog("WARN", message);
+	public static void Warn(string message, string category = null)
+		=> AddLog("WARN", message, category);
 
-	public void Error(string message)
-		=> AddLog("ERROR", message);
+	public static void Error(string message, string category = null)
+		=> AddLog("ERROR", message, category);
 
-	void AddLog(string severity, string message)
+	static void AddLog(string severity, string message, string category)
 	{
-		var prefix = $"{DateTime.Now} [{severity}]".PadRight(31);
-		var formattedMessage = $"{prefix} {message}";
+		if (Instance == null)
+		{
+			CreateNewInstance();
+			Debug("New instance was just created, this should not have happened", "logger");
+		}
 
-		lock (LogWriter)
+		var prefix = $"{DateTime.Now} [{severity}]".PadRight(31);
+		var formattedMessage = string.IsNullOrEmpty(category)
+			? $"{prefix} | {message}"
+			: $"{prefix} | {category} -> {message}";
+
+		lock (Instance.LogWriter)
 		{
 			Console.WriteLine(formattedMessage);
-			LogWriter.WriteLine(formattedMessage);
+			Instance.LogWriter.WriteLine(formattedMessage);
 		}
 	}
 }
