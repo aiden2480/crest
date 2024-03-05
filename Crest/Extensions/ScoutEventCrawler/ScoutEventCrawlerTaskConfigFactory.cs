6a92fbeee684aeb2270231582b993ac09f7232d2
Aiden Gardner <aiden.gardner@wisetechglobal.com>
2024-03-06 10:58:23 +1100

    ScoutEventCrawlerTaskConfigFactory validates jandiUrl

--- Crest/Extensions/ScoutEventCrawler/ScoutEventCrawlerTaskConfigFactory.cs
+++ Crest/Extensions/ScoutEventCrawler/ScoutEventCrawlerTaskConfigFactory.cs
@@ -1,10 +1,13 @@
 ﻿using Crest.Integration;
 using Crest.Utilities;
+using System.Text.RegularExpressions;
 
 namespace Crest.Extensions.TerrainApprovals;
 
 class ScoutEventCrawlerTaskConfigFactory : ITaskConfigFactory
 {
+	readonly Regex JandiWebhookRegex = new(@"^https:\/\/wh\.jandi\.com\/connect-api\/webhook\/\d+\/\w+$");
+
 	public IEnumerable<ITaskConfig> GetValidConfigs(ApplicationConfiguration applicationConfig)
 	{
 		if (applicationConfig.ScoutEventCrawler is null) yield break;
@@ -17,6 +20,20 @@ class ScoutEventCrawlerTaskConfigFactory : ITaskConfigFactory
 			if (!taskConfig.SubscribedRegions.Any())
 			{
 				Logger.Warn("This task does not have any subscribed regions", taskConfig.TaskName);
+				continue;
+			}
+
+			if (!JandiWebhookRegex.IsMatch(taskConfig.JandiUrl))
+			{
+				Logger.Warn($"Jandi URL is in an unexpected format: '{taskConfig.JandiUrl}'", taskConfig.TaskName);
+				continue;
+			}
+
+			var isJandiWebhookUrlValid = JandiAPIClient.IsValidIncomingWebhookURL(taskConfig.JandiUrl);
+
+			if (!isJandiWebhookUrlValid)
+			{
+				Logger.Warn("Jandi URL matches expected url format but is invalid - has URL been set correctly?", taskConfig.TaskName);
 				continue;
 			}
 
